c97b9aaedf62a5832eb785c68af7e7a671c45aa4
aditya <adityagawraw006@gmail.com>
2024-02-08 12:02:51 +0530

    added Base2 class for using testng

--- src/test/java/io/urban/Base2.java
+++ src/test/java/io/urban/Base2.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.io.FileHandler;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -19,8 +20,115 @@ import java.util.List;
 
 public class Base2 {
     WebDriver driver = null;
-    public static void main(String[] args) throws InterruptedException, IOException {
-        new Base().testMethod(5000, 20000, "chrome");
+    Actions actions;
+    WebDriverWait wait;
+//    public static void main(String[] args) throws InterruptedException, IOException {
+//        new Base().testMethod(5000, 20000, "chrome");
+//    }
+
+    @BeforeTest
+    public void setWebDriver(){
+        driver = new ChromeDriver();
+        actions = new Actions(driver);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+    }
+
+    @Test(priority = 0)
+    public void navigateToWebsite(){
+        driver.navigate().to("https://www.urbanladder.com/");
+        driver.manage().window().maximize();
+    }
+
+    @Test(priority = 1)
+    public void navigateToCoffeeTableProducts(){
+        try {
+            Action goToLiving = actions
+                    .moveToElement(driver.findElement(By.cssSelector(".topnav_item.livingunit")))
+                    .build();
+            goToLiving.perform();
+
+            WebElement coffeTable = driver
+                    .findElement(By.cssSelector("a[href^='/coffee-table?src=g_topnav_living_tables_coffee-tables']"));
+            wait.until(ExpectedConditions.elementToBeClickable(coffeTable));
+
+            coffeTable.click();
+        }
+        catch (Exception e){
+            String screenShotPath= "/home/aditya/Documents/"+new Date().toString()+".jpg";
+            TakesScreenshot takesScreenshot =(TakesScreenshot) driver;
+            File screenShot = takesScreenshot.getScreenshotAs(OutputType.FILE);
+            System.out.println("screen "+screenShot.getTotalSpace());
+            try{
+                FileHandler.copy(screenShot, new File(screenShotPath));
+            }
+            catch (IOException er){
+                System.out.println(er);
+            }
+            driver.close();
+        }
+        }
+    @Test(priority = 2)
+    public void setProductPriceRange(){
+        int lowerRange = 5000;
+        int upperRange = 20000;
+        Action goToPrice = actions
+                .moveToElement(driver.findElement(By.cssSelector("li[data-group='price']"))).
+                build();
+
+        wait.until(ExpectedConditions.
+                visibilityOf(driver.findElement(By.cssSelector(".close-reveal-modal.hide-mobile"))));
+        WebElement closePopup = driver.findElement(By.cssSelector(".close-reveal-modal.hide-mobile"));
+        closePopup.click();
+
+        goToPrice.perform();
+
+        WebElement priceRange = driver
+                .findElement(By
+                        .cssSelector(".range-slider.noUi-target.noUi-ltr.noUi-horizontal.noUi-background"));
+
+        int maxPriceRange = Integer.parseInt(priceRange.getAttribute("data-max"));
+        int minPriceRange = Integer.parseInt(priceRange.getAttribute("data-min"));
+        int range = maxPriceRange - minPriceRange;
+
+
+        WebElement scrollSegment = driver.findElement(By.cssSelector(".noUi-base"));
+        wait.until(ExpectedConditions.visibilityOf(scrollSegment));
+
+        int scrollSegementLength = scrollSegment.getSize().getWidth();
+        int leftScrollDistance = (lowerRange * scrollSegementLength) / range;
+        int rightScrollDistance = scrollSegementLength - (upperRange * scrollSegementLength) / range;
+
+        WebElement leftScrolls = driver.findElement(By.cssSelector(".noUi-handle.noUi-handle-lower"));
+        WebElement rightScrolls = driver.findElement(By.cssSelector(".noUi-handle.noUi-handle-upper"));
+
+        actions.dragAndDropBy(leftScrolls, leftScrollDistance, 0).perform();
+        actions.dragAndDropBy(rightScrolls, -rightScrollDistance, 0).perform();
+
+    }
+
+    @Test(priority = 3)
+    public void setTheProductList(){
+        WebElement listOfProduct = driver.
+                findElement(By.
+                        cssSelector(".productlist.withdivider.clearfix.coffee_tables.productgrid"));
+
+        wait.until(ExpectedConditions.stalenessOf(listOfProduct));
+
+        List<WebElement> products = driver.findElements(By.cssSelector("a[class='product-title-block']"));
+        List<Product> productData = new ArrayList<>();
+
+        for (WebElement element : products) {
+            Product product = new Product();
+            product.setProductName(element.findElement(By.cssSelector("span[class = 'name']")).getText());
+            product.setFinalPrice(element.findElement(By.cssSelector("div[class = 'price-number'] span")).getText());
+
+            productData.add(product);
+        }
+
+        ExcelUtil excelUtil = new ExcelUtil();
+        excelUtil.enterProductData(productData);
+
+        driver.close();
     }
 
 
@@ -35,8 +143,7 @@ public class Base2 {
             driver = new FirefoxDriver();
         }
 
-        driver.navigate().to("https://www.urbanladder.com/");
-        driver.manage().window().maximize();
+
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 
